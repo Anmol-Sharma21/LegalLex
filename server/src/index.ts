05b576d18dbd36478a2d65c3cc5a2fc6fdaa9525
Anmol Sharma <anmolsharma@Anmols-MacBook-Pro.local>
2024-02-10 23:59:17 +0530

    connected database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
+import db from './database/db';
+
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => { 
     res.send('Listening');
 })
 
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
     console.log('listening on port 3000');
 })
+
+db(); // Connect to the database
